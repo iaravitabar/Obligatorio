5b9e0d0b50b6c2fe88f8ac4a693fe2a3263b2512
Iara <iara.vitabarhuber@gmail.com>
2024-11-07 22:08:39 -0300

    metodos instructores

--- main.py
+++ main.py
@@ -32,6 +32,50 @@ async def get_instructors(db: Session = Depends(get_db)):
         raise HTTPException(status_code=404, detail="No se encontraron instructores")
     return instructors
 
+@app.post("/instructores/", response_model=InstructorCreate)
+async def create_instructor(instructor: InstructorCreate, db: Session = Depends(get_db)):
+    db_instructor = db.query(Instructor).filter(Instructor.ci == instructor.ci).first()
+    if db_instructor:
+        raise HTTPException(status_code=400, detail="Instructor con esta CI ya existe")
+    new_instructor = Instructor(
+        ci=instructor.ci,
+        nombre=instructor.nombre,
+        apellido=instructor.apellido
+    )
+    db.add(new_instructor)
+    db.commit()
+    db.refresh(new_instructor)
+    return new_instructor
+
+#ins x ci
+@app.get("/instructores/{ci}")
+async def get_instructor(ci: str, db: Session = Depends(get_db)):
+    instructor = db.query(Instructor).filter(Instructor.ci == ci).first()
+    if not instructor:
+        raise HTTPException(status_code=404, detail="Instructor no encontrado")
+    return instructor
+
+@app.put("/instructores/{ci}")
+async def update_instructor(ci:str, updated_data: InstructorCreate, db: Session = Depends(get_db)):
+    instructor = db.query(Instructor).filter(Instructor.ci == ci).first()
+    if not instructor:
+        raise HTTPException(status_code=404, detail="Instructor no encontrado")
+    
+    instructor.nombre = updated_data.nombre
+    instructor.apellido = updated_data.apellido
+    
+    db.commit()
+    db.refresh(instructor)
+    return instructor
+
+@app.delete("/instructores/{ci}")
+async def delete_instructor(ci: str, db: Session = Depends(get_db)):
+    instructor = db.query(Instructor).filter(Instructor.ci == ci).first()
+    if not instructor:
+        raise HTTPException(status_code=404, detail="Instructor no encontrado")
+    db.delete(instructor)
+    db.commit()
+    return {"message": "Instructor eliminado exitosamente"}
 
 ######################################################################
 #                         Rutas de Turnos                           #
